9d64dcf44066c190ed3a37932f2fae87498970f1
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-12-14 16:23:25 +0900

    refactor : 폴더정리 및 파일메타데이터 클래스 추가

---
+++ checkFileContent/NonFormClasses/FileMetaData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkFileContent
+{
+    internal class FileMetaData
+    {
+        public string FilePath { get; private set; }
+        public string FileName { get; private set; }
+        public string FileNameWithoutExtension { get; private set; }
+        public string Extension { get; private set; }
+        public byte[] FileData { get; private set; }
+
+        public FileMetaData(string filePath)
+        {
+            FilePath = filePath;
+            FileName = Path.GetFileName(filePath);
+            FileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+            Extension = Path.GetExtension(filePath);
+            FileData = File.ReadAllBytes(filePath); // 주의: 대용량 파일의 경우 성능 저하 발생 가능
+        }
+
+    }
+}
